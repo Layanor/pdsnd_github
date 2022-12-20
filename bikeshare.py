f3ac2cf0b72c1c209ee786b2418023734dea9ceb
Layanor <98930504+Layanor@users.noreply.github.com>
2022-12-21 00:52:29 +0300

    refactor:edit the restart massage

--- bikeshare.py
+++ bikeshare.py
@@ -196,7 +196,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         display_data(df)
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to start again? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
