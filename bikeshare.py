45093f6f2fbd73a8c4e658f949b2b738f259602e
Layanor <98930504+Layanor@users.noreply.github.com>
2022-12-21 00:38:05 +0300

    docs: add goodbey massage

--- bikeshare.py
+++ bikeshare.py
@@ -199,6 +199,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
+   print ("Have a nice day!")
 
 
 if __name__ == "__main__":
